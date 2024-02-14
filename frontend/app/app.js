1a40a36106b05c6ca25a29beb8de5333b9a66b1b
nelo <nelokwaz@gmail.com>
2024-02-14 14:36:50 +0100

    feat: add templates and static assets

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const router = require("../routes/router");
 
 const app = express();
 
@@ -11,10 +12,11 @@ app.use(express.json());
 
 //middleware templating
 app.set("view engine", "ejs");
-app.engine("ejs", require(ejs).__express);
+app.engine("ejs", require("ejs").__express);
 
 //static site for middleware use
 app.use(express.static("public"));
 app.use(express.static("views"));
 
+app.use("/", router);
 module.exports = app;

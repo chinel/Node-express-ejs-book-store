858fec80bb11385b3147b5ac7e5c80d79b337fbf
nelo <nelokwaz@gmail.com>
2024-02-14 08:41:31 +0100

    feat: setup ejs as a template

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+//cors middleware
+app.use(cors());
+
+//json request
+app.use(express.json());
+
+//middleware templating
+app.set("view engine", "ejs");
+app.engine("ejs", require(ejs).__express);
+
+//static site for middleware use
+app.use(express.static("public"));
+app.use(express.static("views"));
 
 module.exports = app;
